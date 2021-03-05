15def6a1c5e9e3dc0764d79154cc1d275dd20060
Gheorghe Morari <gmorari404@gmail.com>
2021-03-05 18:12:08 +0200

    add todo

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import matplotlib.pyplot as plt
+
+
+#TODO de definit array-uri
+
+array = []
+x_ax = [1,2,3,4,5]
+y_ax = [2,2,3,6,1]
+
+#TODO de citit din fisier array-uri
+
+#TODO de citit din csv array-uri (mai complicat)
+
+#TODO de facut un plot cu subploturi
+#TODO de facut a doua figura fara subploturi
+#TODO fiecare plot si subplot sa fie de tip diferit
+
+plt.plot(x_ax,y_ax)
 plt.show()
